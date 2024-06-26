11f60188625a234d49b16412266e39d7cf66ca1a
Md Econozzaman Econ <92664558+ezecon@users.noreply.github.com>
2024-06-26 20:55:29 +0600

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://mdeconozzama:<EcoN>@cluster0.afxbgyv.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect('mongodb+srv://mdeconozzama:<EcoN>@cluster0.afxbgyv.mongodb.net/mydatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
